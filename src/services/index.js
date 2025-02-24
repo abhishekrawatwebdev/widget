75abb40c79731e66f0eacf6af22b705cc1421ce9
Abhishek rawat <abhishekrawat9956088862@gmail.com>
2025-02-24 19:57:32 +0530

    Update index.js

--- src/services/index.js
+++ src/services/index.js
@@ -1,7 +1,7 @@
 import { get } from "../utils/http";
 
 export const fetchConfigByDomain = (domain) => {
-  let url = `${process.env.REACT_APP_ZITHARA_SERVER_BASE_URL}/public/help-widget/${domain}?forceFetch=true`;
+  let url = `${process.env.REACT_APP_SERVER_BASE_URL}/public/help-widget/${domain}?forceFetch=true`;
   
   return get(url);
 };
